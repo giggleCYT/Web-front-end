7b12a97ede0908e9fb913c52b702ce0d107c6d00
giggleCYT <1655721299@qq.com>
2020-04-22 20:47:16 +0800

    webpack

---
+++ vue/webpack-study/src/main.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+$(function() {
+    $('li:odd').css('backgroundColor', 'pink');
+    $('li:even').css('backgroundColor', function() {
+        return '#' + 'D97634'
+    })
+})
