cd416ce6db14fa592609b11a0b0314f1c30e2c9c
giggleCYT <1655721299@qq.com>
2020-04-24 19:31:20 +0800

    master

--- vue/webpack-study/src/main.js
+++ vue/webpack-study/src/main.js
@@ -1,7 +1,8 @@
 import $ from 'jquery';
 import './css/index.css';
 import './css/index.less';
-import './css/index.scss'
+import './css/index.scss';
+import '_bootstrap@3.3.7@bootstrap/dist/css/bootstrap.css'
 $(function() {
     $('li:odd').css('backgroundColor', 'yellow');
     $('li:even').css('backgroundColor', function() {
--- vue/webpack-study/webpack.config.js
+++ vue/webpack-study/webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
     module: {
         rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
-            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] }
+            { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'] },
+            { test: /\.(eot|svg|ttf|woff|woff2)$/, use: 'url-loader' }
         ]
     }
 }
