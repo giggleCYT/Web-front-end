a34744644282ff45cb94bb1689709f82058cdffd
giggleCYT <1655721299@qq.com>
2020-05-06 18:32:49 +0800

    角色列表

--- vue/pc/src/components/power/Roles.vue
+++ vue/pc/src/components/power/Roles.vue
@@ -5,16 +5,109 @@
             <el-breadcrumb-item>权限管理</el-breadcrumb-item>
             <el-breadcrumb-item>角色列表</el-breadcrumb-item>
         </el-breadcrumb>
-        <el-card></el-card>
+        <el-card>
+            <el-row>
+                 <el-col >
+                     <el-button type="primary" @click="addDialogVisible=true">添加角色</el-button>
+                 </el-col>
+             </el-row>
+
+             <el-table :data="rolesList" stripe border>
+                <el-table-column type="expand">
+                    <template slot-scope="scope">
+                        <el-row v-for="(item1,i1) in scope.row.children" :key="item1.id" :class="['bdbottom',i1===0?'bdtop':'','vcenter']">
+                            <el-col :span=5>
+                                <el-tag closable @close="removeUserById(scope.row,item1.id)">{{item1.authName}}</el-tag>
+                                <i class="el-icon-caret-right"></i>
+                            </el-col>
+                            <el-col :span=19>
+                                <el-row v-for="(item2,i2) in item1.children" :key="item2.id" :class="[i2===0?'':'bdtop','vcenter']">
+                                    <el-col :span=6>
+                                        <el-tag type="success" closable @close="removeUserById(scope.row,item2.id)">{{item2.authName}}</el-tag>
+                                        <i class="el-icon-caret-right"></i>
+                                    </el-col>
+                                    <el-col :span=18>
+                                        <el-tag type="warning" v-for="(item3) in item2.children" :key="item3.id" closable @close="removeUserById(scope.row,item3.id)">
+                                            {{item3.authName}}
+                                        </el-tag>
+                                    </el-col>
+                                </el-row>
+                            </el-col>
+                        </el-row>
+                    </template>
+                </el-table-column>
+
+                <el-table-column type="index"></el-table-column>
+                <el-table-column label="角色名称" prop="roleName"></el-table-column>
+                <el-table-column label="角色描述" prop="roleDesc"></el-table-column>
+
+                 <el-table-column label="操作" width="300px">
+                     <template>
+                             <el-button type="primary" icon="el-icon-edit" size="mini" >编辑</el-button>
+                             <el-button type="danger" icon="el-icon-delete" size="mini" >删除</el-button>
+                             <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+                     </template>
+                 </el-table-column>
+        </el-table>
+        </el-card>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data () {
+        return {
+            rolesList:[]
+        }
+    },
+    created () {
+        this.getRolesList()
+    },
+    methods: {
+        async getRolesList(){
+            const {data:res}=await this.$http.get('roles')
+            if(res.meta.status!==200){
+                return this.$message.error('获取角色列表失败')
+            }
+            this.rolesList=res.data
+            console.log(this.rolesList);
+            
+        },
+        async removeUserById(role,rightid){
+            const confirmResult=await this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).catch(err=>err)
+            if(confirmResult!=='confirm'){
+                return this.$message.info('已经取消删除')
+            }
+            const {data:res}=await this.$http.delete(`roles/${role.id}/rights/${rightid}`)
+            if(res.meta.status!==200){
+                return this.$message.error('用户删除失败')
+            }
+            this.$message.success('用户删除成功')
+            // this.getList()
+            role.children=res.data
+        
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
-
+.el-tag{
+    margin: 7px;
+}
+.bdtop{
+    border-top: 1px solid #eee;
+}
+.bdbottom{
+    border-bottom: 1px solid #eee;
+}
+.vcenter{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
 </style>
