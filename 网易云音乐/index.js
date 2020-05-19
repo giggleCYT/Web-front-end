a0c687e5250bec78b3ab1e6f8ec18fd34caf495d
giggleCYT <1655721299@qq.com>
2020-05-19 17:05:30 +0800

    master

--- 网易云音乐/index.js
+++ 网易云音乐/index.js
@@ -1,5 +1,15 @@
-// window.onload = function() {
-//     var footerIcnos = document.querySelector('.footer-icnos')
-//     console.log(footerIcnos.children);
+window.onload = function() {
+    axios.defaults.baseURL = 'https://autumnfish.cn';
 
-// }
+    function fn() {
+        axios.get('/top/playlist?limit=10&order=new')
+            .then(function(res) {
+                console.log(res);
+            }).catch(function(error) {
+                console.log('获取失败！');
+
+            })
+    }
+    fn()
+
+}
